cc76d33254753f28bad2cf4647fc9cedf8b2a56e
alohahi <33651118+Dankangdongwon@users.noreply.github.com>
2023-11-12 23:11:45 +0000

    Create common.py

---
+++ common.py
@@ -0,0 +1,67 @@
+def start_client(message, sip, sport):
+    # 소켓 객체 생성 (IPv4, TCP)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # 서버 IP 주소와 포트 번호
+    server_address = (sip, sport)
+
+    # 서버에 연결 시도
+    client_socket.connect(server_address)
+    print(f"connect: {server_address}")
+
+    # 서버로 데이터 송신
+    client_socket.sendall(message.encode('utf-8'))
+    print(f"send: {message}")
+
+    response = client_socket.recv(1024).decode('cp949')
+    print(f"return message: {response}")
+
+    # 연결 종료
+    client_socket.close()
+    print("connection closed.")
+    return response
+
+def ji_calc(i, j):
+    value_size = [4,1,1,2,20,12,40,32,13,14,4,100,20,137]
+    return i + value_size[j], j + 1
+
+def conv_rcv(res):
+    original_string = res
+    value_size = [4,1,1,2,20,12,40,32,13,14,4,100,20,137]
+
+    value_dic = {}
+    i = 0
+    j = 0
+    value_dic['Ver'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['CryptGB'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['KeySeq'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['MsgType'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['RecKey'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['SvcId'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['MchtTrId'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['PrdtPrice'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['MobilId'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['ActDate'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['ResultCD'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['ResultMsg'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['AuthCode'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc(i, j)
+    value_dic['Filler'] = original_string[i:i+value_size[j]]
+    print(value_dic)
+    return value_dic
+
+def enc_value(val, SvcId):
+    CryptKey = SvcId[:-4]+SvcId[:-4]
+    return str(McashSeed.encodeString(val, CryptKey.encode()))
