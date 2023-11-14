bedb60eb672a2d62e89a264ceb219ba94e7d121a
alohahi <33651118+Dankangdongwon@users.noreply.github.com>
2023-11-14 04:45:57 +0000

    Update common.py

--- common.py
+++ common.py
@@ -70,3 +70,96 @@ def enc_value(val, SvcId, McashSeed):
 def dec_value(val, SvcId, McashSeed):
     CryptKey = SvcId[:-4]+SvcId[:-4]
     return str(McashSeed.decodeString(val, CryptKey.encode()))
+
+def ji_calc2(i, j):
+    value_size = [4, 1, 1, 2, 2, 20, 12, 3, 50, 32, 1, 8, 4, 75, 15, 6, 15, 14, 10, 1, 1, 1, 15, 1, 1, 1, 20, 10, 10, 2, 20, 10, 50, 10, 47]
+    return i + value_size[j], j + 1
+
+def conv_rcv2(res):
+    original_string = res
+    value_size = [4, 1, 1, 2, 2, 20, 12, 3, 50, 32, 1, 8, 4, 75, 15, 6, 15, 14, 10, 1, 1, 1, 15, 1, 1, 1, 20, 10, 10, 2, 20, 10, 50, 10, 47]
+
+    value_dic = {}
+    i = 0
+    j = 0
+
+    value_dic['Ver'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['CryptGb'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['KeySeq'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['CashGb'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['MsgType'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['RecKey'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['SvcId'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['CommId'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['MchtTrId'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['PrdtPrice'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['EmailFlag'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['Item'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['ResultCD'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['ResultMsg'] = original_string[i:i+value_size[j]].strip()
+    i = i + 25 - (count_korean_characters(value_dic['ResultMsg']))
+    i,j = ji_calc2(i, j)
+    value_dic['PhoneId'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['SmsVal'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['MobilId'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['ActDate'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['RemainAmt'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['CopyFlag'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['SafeGuard'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['AnsimFlag'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['AutoBillKey'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['AuthType'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['EventAgree_Yn'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['Wc499_Join_Stat'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['Wc499_Cust_No'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['OrnDcAmt'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['KenDcAmt'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['Filler'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['EasyKey'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['MobDcAmt'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['PacketNo'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['ExcDdcAmt'] = original_string[i:i+value_size[j]]
+    i,j = ji_calc2(i, j)
+    value_dic['Filler'] = original_string[i:i+value_size[j]]
+#    for s in range(len(value_size)):
+    print(value_dic)
+    return value_dic
+
+def count_korean_characters(s):
+    count = 0
+    for char in s:
+        if '가' <= char <= '힣':
+            count += 1
+    return count
